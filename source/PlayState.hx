1a42c15731b9e58be6060b59a39ed5572abe8af7
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-25 09:58:44 +0100

    Added instance of Player to PlayState

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,21 @@
 package;
 
 import flixel.FlxState;
+import player.Player;
 
 class PlayState extends FlxState
 {
+    /**
+      Our instance of Player
+      **/
+    var player : Player;
+
     override public function create()
     {
         super.create();
+
+        this.player = new Player();
+        add(player);
     }
 
     override public function update(elapsed:Float)
